f378cd7638defd039acbe2409470822868480ce4
aluno <aluno@ifpe>
2024-08-17 16:10:31 -0300

    criado categoria produto. ajustar modelo produto

---
+++ src/main/java/br/com/ifpe/oxefood/api/produto/CategoriaProdutoController.java
@@ -0,0 +1,60 @@
+package br.com.ifpe.oxefood.api.produto;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.ifpe.oxefood.modelo.produto.CategoriaProdutoService;
+import br.com.ifpe.oxefood.modelo.produto.CategoriaProduto;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categoriaproduto")
+@CrossOrigin
+public class CategoriaProdutoController {
+
+    @Autowired
+    private CategoriaProdutoService categoriaProdutoService;
+
+    @PostMapping
+    public ResponseEntity<CategoriaProduto> save(@RequestBody CategoriaProdutoRequest request) {
+
+        CategoriaProduto categoriaProduto = categoriaProdutoService.save(request.build());
+        return new ResponseEntity<CategoriaProduto>(categoriaProduto, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<CategoriaProduto> listarTodos() {
+        return categoriaProdutoService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public CategoriaProduto obterPorID(@PathVariable Long id) {
+        return categoriaProdutoService.obterPorID(id);
+    }
+
+
+    @PutMapping("/{id}")
+ public ResponseEntity<CategoriaProduto> update(@PathVariable("id") Long id, @RequestBody CategoriaProdutoRequest request) {
+
+    categoriaProdutoService.update(id, request.build());
+       return ResponseEntity.ok().build();
+ }
+
+ @DeleteMapping("/{id}")
+   public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+    categoriaProdutoService.delete(id);
+       return ResponseEntity.ok().build();
+   }
+}
---
+++ src/main/java/br/com/ifpe/oxefood/modelo/produto/CategoriaProduto.java
@@ -0,0 +1,29 @@
+package br.com.ifpe.oxefood.modelo.produto;
+
+import org.hibernate.annotations.SQLRestriction;
+
+import br.com.ifpe.oxefood.util.entity.EntidadeAuditavel;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "CategoriaProduto")
+@SQLRestriction("habilitado = true")
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class CategoriaProduto extends EntidadeAuditavel {
+
+    @Column
+    private String descricao;
+
+}
